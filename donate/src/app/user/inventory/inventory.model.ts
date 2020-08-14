a035b97f8a7518635195dfd763c7137d310c4afd
siddharthjn <siddharth.smu@gmail.com>
2020-08-14 17:13:14 -0300

    cleanup

--- donate/src/app/user/inventory/inventory.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Store {
-    name: string;
-    phone: string;
-    streetName: string;
-    streetNumber: string;
-    streetProvince: string;
-    streetCountry: string;
-    streetCity: string;
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { AuthService } from './core/auth.service';
-import { CoreModule } from './core/core.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { InventoryComponent } from './user/inventory/inventory.component';
-import { ItemAlertComponent } from './item-alert/item-alert.component';
-
+/* tslint:disable-next-line:quotemark */
+import { AuthService } from "./core/auth.service";
+import { CoreModule } from "./core/core.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { environment } from "src/environments/environment";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { InventoryComponent } from "./inventory/inventory.component";
+import { ItemAlertComponent } from "./item-alert/item-alert.component";
 
 @NgModule({
   declarations: [
@@ -21,16 +21,16 @@ import { ItemAlertComponent } from './item-alert/item-alert.component';
     NavbarComponent,
     HomeComponent,
     InventoryComponent,
-    ItemAlertComponent
+    ItemAlertComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    CoreModule
+    CoreModule,
   ],
   providers: [AuthService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
